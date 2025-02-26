76ec85e49c820f782297bc4854c8da70aaa76628
bjropguar <112531454+JetOpguar@users.noreply.github.com>
2025-02-26 09:25:58 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Simulate a delay (e.g., fetching data or loading resources)
-    setTimeout(showContent, 4000); // Loader will disappear after 5 seconds (5000 milliseconds)
+    setTimeout(showContent, 100); // Loader will disappear after 5 seconds (5000 milliseconds)
 });
 
 // Highlight active section in navbar on scroll
@@ -255,7 +255,6 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener("contextmenu", function(event) {
     event.preventDefault();
 });
-
 document.addEventListener("keydown", function(event) {
     // Disable F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U
     if (
