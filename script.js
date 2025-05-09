f097a73c8006cba09d3ff0a0ad1f3fce58288452
bjropguar <112531454+JetOpguar@users.noreply.github.com>
2025-05-09 16:34:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Simulate a delay (e.g., fetching data or loading resources)
-    setTimeout(showContent, 100); // Loader will disappear after 5 seconds (5000 milliseconds)
+    setTimeout(showContent, 4000); // Loader will disappear after 5 seconds (5000 milliseconds)
 });
 
 // Highlight active section in navbar on scroll
@@ -252,9 +252,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
-document.addEventListener("contextmenu", function(event) {
-    event.preventDefault();
-});
+
 document.addEventListener("keydown", function(event) {
     // Disable F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U
     if (
